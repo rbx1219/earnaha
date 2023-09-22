4bb43cd25be98857e0c259ff00ca5e06f2b755ad
Chuan-Che <b95056@csie.ntu.edu.tw>
2023-09-22 23:35:48 +0800

    app infra

--- earnaha/src/app.module.ts
+++ earnaha/src/app.module.ts
@@ -1,9 +1,33 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { DashboardModule } from './dashboard/dashboard.module';
+import { MailModule } from './mail/mail.module';
+import { RedisModule } from './redis/redis.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.POSTGRES_HOST,
+      port: +process.env.POSTGRES_PORT,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.POSTGRES_DB,
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: false,
+    }),
+    AuthModule,
+    RedisModule,
+    UserModule,
+    MailModule,
+    DashboardModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- earnaha/src/main.ts
+++ earnaha/src/main.ts
@@ -1,8 +1,27 @@
+// main.ts
+
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as cookieParser from 'cookie-parser';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Enable CORS for all routes and origins
+  app.use(cookieParser());
+
+  const config = new DocumentBuilder()
+    .setTitle('Aha exam api page')
+    .setDescription('Swagger page for api reference')
+    .setVersion('1.0')
+    .addTag('auth-page')
+    .addServer('/webapi')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('doc', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
